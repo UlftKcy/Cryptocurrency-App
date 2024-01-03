bccae7ea6cd0634d1973846a00e3ef187e983e82
ukacay <ukacay87@gmail.com>
2024-01-03 23:10:30 +0300

    updated home page and coins table

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CoinDetail from "./pages/CoinDetail";
+import { Layout } from "antd";
+import { CSSProperties } from "react";
+
+const { Header, Content } = Layout;
+
+const headerStyle: CSSProperties = {
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  fontSize: "1.2rem",
+  letterSpacing: "1px",
+};
+
+const layoutStyle = {
+  overflow: "hidden",
+  width: "100%",
+  minHeight: "100vh",
+};
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path=":uuid" element={<CoinDetail/>}/>
-      </Routes>
-    </Router>
-  )
+    <Layout style={layoutStyle}>
+      <Content>
+        <Router>
+          <Header style={headerStyle}>
+            <Link to="/" relative="path">
+              CoinRanking
+            </Link>
+          </Header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path=":uuid" element={<CoinDetail />} />
+          </Routes>
+        </Router>
+      </Content>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/components/CoinTable.tsx
+++ src/components/CoinTable.tsx
@@ -1,19 +1,61 @@
 import Column from "antd/es/table/Column";
 import { CoinType } from "../types";
-import { Button, Table } from "antd";
+import { Button, Flex, Image, Space, Table } from "antd";
 
 export default function CoinTable({ data }: { data: CoinType[] | undefined }) {
   return (
     <Table dataSource={data}>
-      <Column title="Name" dataIndex="name" key="name" />
-      <Column title="Price" dataIndex="price" key="price" />
-      <Column title="MarketCap" dataIndex="marketcap" key="marketcap" />
-      <Column title="24h" dataIndex="change" key="change" />
+      <Column
+        title="Name"
+        dataIndex="name"
+        key="name"
+        render={(_: undefined, record: CoinType) => (
+          <Space align="center" size="middle">
+            <span>{record.rank}</span>
+            <Image src={record.iconUrl} width={25} height={25}/>
+            <Flex vertical>
+              <h4 style={{ fontWeight: "bold" }}>{record.name}</h4>
+              <span>{record.symbol}</span>
+            </Flex>
+          </Space>
+        )}
+      />
+      <Column
+        title="Price"
+        dataIndex="price"
+        key="price"
+        render={(_: undefined, record: CoinType) => (
+          <span style={{ fontWeight: "bold" }}>
+            $ {Number(record.price).toFixed(2)}
+          </span>
+        )}
+      />
+      <Column
+        title="MarketCap"
+        dataIndex="marketCap"
+        key="marketCap"
+        render={(_: undefined, record: CoinType) => (
+          <span>
+            $ {(Number(record.marketCap) / 1000000000).toFixed(2)} billion
+          </span>
+        )}
+      />
+      <Column
+        title="24h"
+        dataIndex="change"
+        key="change"
+        render={(_: undefined, record: CoinType) => {
+          const styleChange = {
+            color: Number(record.change) > 0 ? "green" : "red",
+          };
+          return <span style={styleChange}>{record.change} %</span>;
+        }}
+      />
       <Column
         title="Detail"
         key="detail"
         render={(record: CoinType) => (
-          <Button type="link" href={record.uuid}>
+          <Button type="link" href={record.uuid} style={{ paddingLeft: 0 }}>
             Show Detail
           </Button>
         )}
