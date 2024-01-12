10338b8ffae2d6bb9305899f5aeb9fc3da57958a
ukacay <ukacay87@gmail.com>
2024-01-13 00:37:24 +0300

    added chart

--- src/pages/CoinDetail.tsx
+++ src/pages/CoinDetail.tsx
@@ -1,16 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinByUuid } from "../utils/service/api";
 import { useParams } from "react-router-dom";
-import { CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from "chart.js";
+import Chart from 'chart.js/auto';
+import { CategoryScale, Legend, LinearScale, Title, Tooltip,PointElement } from "chart.js";
+import { Line } from "react-chartjs-2";
 
-
-ChartJS.register(
+Chart.register(
   Title,
   Tooltip,
   Legend,
   CategoryScale,
   LinearScale,
+  PointElement
 );
+const labels = ["January", "February", "March", "April", "May", "June"];
+
+const dataChart = {
+  label:labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [10, 30, 50, 20, 25, 44],
+      backgroundColor: ['red', 'green', 'pink', 'orange', 'yellow', 'lime'],
+      borderColor: "rgb(255, 99, 132)",
+      borderWidth: 1,
+    }
+  ]
+};
 
 export default function CoinDetail() {
   const { uuid } = useParams();
@@ -30,5 +46,7 @@ export default function CoinDetail() {
     return <span>Error: {error.message}</span>;
   }
 
-  return <div>{data?.name}</div>;
+  return (
+    <Line data={dataChart}/>
+  );
 }
